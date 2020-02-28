e8b5d5632adf034f5d7514c7c1d580005bed61b5
Sara Khandaker <kttsara@hotmail.com>
2020-02-28 01:10:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,4 @@
-
-source_array=[]
-
-map_to_negativize(source_array)
+def map_to_negativize(source_array)
 i=0
 newarr=[]
 while i< source_array.length
@@ -11,11 +8,11 @@ end
 new arr
 end
 
-map_to_no_change(source_array)
+def map_to_no_change(source_array)
 source_array
 end
 
-map_to_double(source_array)
+def map_to_double(source_array)
 i=0
 newarr=[]
 while i< source_array.length
@@ -25,7 +22,7 @@ end
 new arr
 end
 
-map_to_square(source_array)
+def map_to_square(source_array)
 i=0
 newarr=[]
 while i< source_array.length
@@ -35,7 +32,7 @@ end
 new arr
 end
 
-reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point)
 i=0
 total=starting_point
 while i< source_array.length
@@ -45,11 +42,11 @@ end
 total
 end
 
-reduce_to_all_true(source_array)
+def reduce_to_all_true(source_array)
 true
 end
 
-reduce_to_any_true(source_array)
+def reduce_to_any_true(source_array)
 i=0
 value=false
 while i< source_array.length
