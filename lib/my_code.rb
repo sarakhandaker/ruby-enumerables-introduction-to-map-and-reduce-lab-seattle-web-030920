d26cdc70b91c590b4f7692ae96402c5df5f77aea
Sara Khandaker <kttsara@hotmail.com>
2020-02-28 00:50:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,23 +1,56 @@
 # My Code here....
 
 map_to_negativize(source_array)
-
+i=0
+newarr=[]
+while i< source_array.length
+newarr[i]= -1*source_array[i]
+i+=1
+end
+new arr
 end
 
 map_to_no_change(source_array)
-
+i=0
+newarr=source_array
+new arr
 end
 
 map_to_double(source_array)
-
+i=0
+newarr=[]
+while i< source_array.length
+newarr[i]= 2*source_array[i]
+i+=1
+end
+new arr
 end
 
 map_to_square(source_array)
-
+i=0
+newarr=[]
+while i< source_array.length
+newarr[i]= source_array[i]*source_array[i]
+i+=1
+end
+new arr
 end
 
 reduce_to_total(source_array, starting_point)
-
+i=0
+total=0
+while i< source_array.length
+total+= source_array[i]
+i+=1
 end
+total
+end
+
+
 reduce_to_all_true(source_array)
+true
+end
+
 reduce_to_any_true(source_array)
+true
+end
