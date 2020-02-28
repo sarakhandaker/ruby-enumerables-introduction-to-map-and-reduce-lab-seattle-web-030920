2de0759a9522753e5a46e6cc559a1540f5ec7618
Sara Khandaker <kttsara@hotmail.com>
2020-02-28 01:17:04 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,64 @@
-# My Code here....
+def map_to_negativize(source_array)
+i=0
+newarr=[]
+while i< source_array.length
+newarr[i]= -1*source_array[i]
+i+=1
+end
+newarr
+end
+
+def map_to_no_change(source_array)
+source_array
+end
+
+def map_to_double(source_array)
+i=0
+newarr=[]
+while i< source_array.length
+newarr[i]= 2*source_array[i]
+i+=1
+end
+newarr
+end
+
+def map_to_square(source_array)
+i=0
+newarr=[]
+while i< source_array.length
+newarr[i]= source_array[i]*source_array[i]
+i+=1
+end
+newarr
+end
+
+def reduce_to_total(source_array, starting_point=0)
+i=0
+total=starting_point
+while i< source_array.length
+total+= source_array[i]
+i+=1
+end
+total
+end
+
+def reduce_to_all_true(source_array)
+i=0
+value=false
+while i< source_array.length
+value=true if source_array[i]==true
+value=false if source_array[i]==false
+i+=1
+end
+value
+end
+
+def reduce_to_any_true(source_array)
+i=0
+value=false
+while i< source_array.length
+value=true if source_array[i]==true
+i+=1
+end
+value
+end
